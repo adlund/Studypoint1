2655890175c2b4a4066ee39ac916e4f078265a61
Alex <Alex@Adlund>
2015-02-06 14:29:28 +0100

    Reading Get and Post parameters

--- src/firsthttpserver/FirstHtttpServer.java
+++ src/firsthttpserver/FirstHtttpServer.java
@@ -13,6 +13,7 @@ import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * @author Lars Mortensen
@@ -31,6 +32,7 @@ public class FirstHtttpServer {
         server.createContext("/welcome", new RequestHandler());
         server.createContext("/headers", new RequestHandler2());
         server.createContext("/pages/", new RequestHandler3());
+        server.createContext("/parameters", new RequestHandler4());
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
@@ -122,6 +124,42 @@ public class FirstHtttpServer {
             he.sendResponseHeaders(200, bytesToSend.length);
             try (OutputStream os = he.getResponseBody()) {
                 os.write(bytesToSend, 0, bytesToSend.length);
+
+            }
+        }
+    }
+    static class RequestHandler4 implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            String response;
+            
+            Scanner scan = new Scanner(he.getRequestBody());
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>My fancy Web Site</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            sb.append("<h2>Method is:" + he.getRequestMethod() + "</h2>\n");
+            sb.append("<h2>Get-Parameters is:" + he.getRequestURI().getQuery()+ "</h2>\n");
+            while(scan.hasNext()){
+            sb.append("Request body, with Post-parameters: "+scan.nextLine());
+            sb.append("</br>");
+            }
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            response = sb.toString();
+
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+
+            he.sendResponseHeaders(200, response.length());
+            try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
+                pw.print(response); //What happens if we use a println instead of print --> Explain
 
             }
         }
