7383e07e27a71083f8488fc208f6c014fccfe0ef
Alex <Alex@Adlund>
2015-02-06 14:07:25 +0100

    Added Worlds simplest web-server

--- src/firsthttpserver/FirstHtttpServer.java
+++ src/firsthttpserver/FirstHtttpServer.java
@@ -4,8 +4,11 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import firsthttpserver.FirstHtttpServer.RequestHandler.RequestHandler2;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.util.List;
@@ -27,6 +30,7 @@ public class FirstHtttpServer {
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
         server.createContext("/welcome", new RequestHandler());
         server.createContext("/headers", new RequestHandler2());
+        server.createContext("/pages/", new RequestHandler3());
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
@@ -60,44 +64,65 @@ public class FirstHtttpServer {
 
             }
         }
+    }
 
-        static class RequestHandler2 implements HttpHandler {
+    static class RequestHandler2 implements HttpHandler {
 
-            @Override
-            public void handle(HttpExchange he) throws IOException {
-                String response = null;
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            String response = null;
 
-                Headers h = he.getResponseHeaders();
-                h.add("Content-Type", "text/html");
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
 
-                h = he.getRequestHeaders();
-                StringBuilder sb = new StringBuilder();
-                sb.append("<!DOCTYPE html>\n");
-                sb.append("<html>\n");
-                sb.append("<head>\n");
-                sb.append("<title>Headers</title>\n");
-                sb.append("<meta charset='UTF-8'>\n");
-                sb.append("</head>\n");
-                sb.append("<body>\n");
-                sb.append("<table>\n");
+            h = he.getRequestHeaders();
+            StringBuilder sb = new StringBuilder();
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>Headers</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            sb.append("<table>\n");
 
-                for (Map.Entry<String, List<String>> entry : h.entrySet()) {
+            for (Map.Entry<String, List<String>> entry : h.entrySet()) {
 
-                    sb.append("<tr>\n");
-                    sb.append("<td>" + entry.getKey() + "</td>\n");
-                    sb.append("<td>" + entry.getValue().toString()+ "</td>\n");
-                    sb.append("</tr>\n");
-                }
-                sb.append("</table>\n");
-                sb.append("</body>\n");
-                sb.append("</html>\n");
-                response = sb.toString();
+                sb.append("<tr>\n");
+                sb.append("<td>" + entry.getKey() + "</td>\n");
+                sb.append("<td>" + entry.getValue().toString() + "</td>\n");
+                sb.append("</tr>\n");
+            }
+            sb.append("</table>\n");
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            response = sb.toString();
 
-                he.sendResponseHeaders(200, response.length());
-                try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
-                    pw.print(response); //What happens if we use a println instead of print --> Explain
+            he.sendResponseHeaders(200, response.length());
+            try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
+                pw.print(response); //What happens if we use a println instead of print --> Explain
 
-                }
+            }
+        }
+    }
+
+    static class RequestHandler3 implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+
+            File file = new File("public/" + "html.html");
+            byte[] bytesToSend = new byte[(int) file.length()];
+            try {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytesToSend, 0, bytesToSend.length);
+            } catch (IOException ie) {
+                ie.printStackTrace();
+            }
+            he.sendResponseHeaders(200, bytesToSend.length);
+            try (OutputStream os = he.getResponseBody()) {
+                os.write(bytesToSend, 0, bytesToSend.length);
+
             }
         }
     }
